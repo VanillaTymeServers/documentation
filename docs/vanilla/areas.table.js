608d1fd53b393812ac3c85845d4a725e6efea7cc
FrannyPack_ <fmrivera30@ymail.com>
2023-07-02 12:32:15 -0400

    Update areas.table.js

--- docs/vanilla/areas.table.js
+++ docs/vanilla/areas.table.js
@@ -16,27 +16,27 @@ export const areas = [
   },
   {
     name: "Public Mushroom Island",
-    overworld: "x5300 z1694",
+    overworld: "x5300, z1694",
     nether: "East 665"
   },
   {
     name: "Public Coral Reef",
-    overworld: "x5300 z1694",
+    overworld: "x5300, z1694",
     nether: "East 665"
   },
   {
     name: "Public Mangrove",
-    overworld: "x4340 z1264",
+    overworld: "x4340, z1264",
     nether: "East 545"
   },
   {
     name: "Public Stronghold",
-    overworld: "x192 z2288",
+    overworld: "x192, z2288",
     nether: "south 285"
   },
   {
     name: "Public Ice Spikes",
-    overworld: "x2500 z700",
+    overworld: "x2500, z700",
     nether: "East 315"
   }
 ];
